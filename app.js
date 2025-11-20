a3bcd848dc61c0e5b10aa60cb06349ffa7fe984c
logrcia <lolagm04@gmail.com>
2025-11-19 22:26:26 -0300

    final changes

--- app.js
+++ app.js
@@ -22,10 +22,12 @@ app.set("port", process.env.PORT || 4000);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(cors({
-  origin: process.env.FRONT_URL,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.FRONT_URL,
+    credentials: true
+  })
+);
 
 /* Routes */
 app.use("/", indexRoutes);
